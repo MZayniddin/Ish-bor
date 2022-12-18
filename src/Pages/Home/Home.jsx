6e460d83c58b9c0776b6b1baee72da79b6440065
Jakhongir <jakhongiryusupov58@gmail.com>
2022-12-18 18:07:56 +0500

    header bottom desktop finished

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Layout } from '../../components'
+import React from 'react';
+import { Layout } from '../../components';
+import HeaderBottom from '../../components/Header-Bottom/Header-Bottom';
 
 export default function Home() {
   return (
     <Layout>
-
+      <HeaderBottom />
     </Layout>
   )
 }
